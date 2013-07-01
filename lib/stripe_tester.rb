92176aee02ebadf5494aeb5a1c5fd5e816f0d53e
Luay Bseiso <luay@buttercloud.com>
2013-07-01 17:41:39 +0300

    wrote class methods for loading yaml and for setting up webhook url -- fixes #4, fixes #8

--- lib/stripe_tester.rb
+++ lib/stripe_tester.rb
@@ -1,5 +1,27 @@
 require "stripe_tester/version"
 
 module StripeTester
-  # Your code goes here...
+	# when done test me
+	# run callback with options to customize the json
+	def self.create_event(callback_type, options={})
+		event_json = self.load_template(callback_type)
+		
+		# TODO: change the default value of the json with the set options
+
+		url = URI.parse(webhook_url)
+		Net::HTTP.post_form(url, event_json)
+	end
+
+	# load yaml with specified callback type
+	def self.load_template(callback_type)
+		template = Psych.load_file("./webhooks/#{callback_type}.yml")
+	end
+
+	def self.webhook_url=(url)
+		@url = url
+	end
+
+	def self.webhook_url
+		@url
+	end
 end
