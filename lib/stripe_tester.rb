61cd4a3f0467a700b0a9819d850f1f468f80afd1
Luay Bseiso <luay@buttercloud.com>
2013-07-02 12:23:08 +0300

    add post to url method to send data to user specified url; change global webhook url type to URI object -- fixes #15

--- lib/stripe_tester.rb
+++ lib/stripe_tester.rb
@@ -4,12 +4,31 @@ module StripeTester
 	# when done test me
 	# run callback with options to customize the json
 	def self.create_event(callback_type, options={})
-		event_json = self.load_template(callback_type)
+		data = self.load_template(callback_type)
 		
 		# TODO: change the default value of the json with the set options
+		post_to_url(data)
+	end
 
-		url = URI.parse(webhook_url)
-		Net::HTTP.post_form(url, event_json)
+	def self.post_to_url(data={})
+		post_url = webhook_url
+
+		# set up request
+		req = Net::HTTP.Post.new(post_url.path)
+		req.content_type = 'application/json'
+		req.body = data.to_json
+
+		# send request
+		res = Net::HTTP.start(post_url.hostname, post_url.port) do |http|
+			http.request(req)
+		end
+
+		case res
+		when Net::HTTPSuccess, Net::HTTPRedirection
+			# good
+		else
+			res.value
+		end
 	end
 
 	# load yaml with specified callback type
@@ -17,8 +36,15 @@ module StripeTester
 		template = Psych.load_file("./webhooks/#{callback_type}.yml")
 	end
 
+	# save the url and a URI object
 	def self.webhook_url=(url)
-		@url = url
+		begin
+			temp_url = URI.parse(url)
+		rescue Exception => e
+			puts "Could not parse URL -- #{e}"
+			return
+		end
+		@url = temp_url
 	end
 
 	def self.webhook_url
