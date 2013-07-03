a10c2969583b1a89e9a9b5307517bf4252c2b72a
Luay Bseiso <luay@buttercloud.com>
2013-07-03 19:09:51 +0300

    add hack for searching nested hashes, refs #6 + check if URL is valid before storing it, fixes #16

--- lib/stripe_tester.rb
+++ lib/stripe_tester.rb
@@ -4,13 +4,33 @@ require 'net/http'
 require 'json'
 
 module StripeTester
+
+	# hack to find the value of the nested key (returns only the first one found)
+	def self.deep_fetch(hash, key, default = nil)
+   default = yield if block_given?
+   (deep_find(hash, key) or default) or raise KeyError.new("key not found: #{key}")
+ 	end
+
+ 	# TODO: check in array as well
+ 	def self.deep_find(hash, key)
+		hash.key?(key) ? hash[key] : hash.values.inject(nil) do |memo, v|  
+			v = v.first if v.is_a?(Array) # convert array to hash if any
+			deep_find(v, key) if v.is_a?(Hash)
+		end
+  end
+
 	# when done test me
 	# run callback with options to customize the json
 	def self.create_event(callback_type, options={})
-		data = self.load_template(callback_type)
+		webhook_data = self.load_template(callback_type)
 		
-		# TODO: change the default value of the json with the set options
-		post_to_url(data)
+		override_attributes(webhook_data, options) unless options.empty?
+
+		post_to_url(webhook_data)
+	end
+
+	def self.override_attributes(original_data, new_data)
+
 	end
 
 	def self.post_to_url(data={})
@@ -41,16 +61,17 @@ module StripeTester
 
 	# save the url and a URI object
 	def self.webhook_url=(url)
-		begin
+		if url =~ /^#{URI::regexp}$/
 			temp_url = URI.parse(url)
-		rescue Exception => e
-			puts "Could not parse URL -- #{e}"
-			return
+			@url = temp_url
 		end
-		@url = temp_url
 	end
 
 	def self.webhook_url
 		@url
+	end
+
+	def self.remove_url
+		@url = nil
 	end
 end
