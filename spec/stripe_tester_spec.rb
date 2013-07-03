d50270c09e42f67b05e51b176051343c559438e6
Luay Bseiso <luay@buttercloud.com>
2013-07-03 19:09:08 +0300

    fix tests -- refs #14

--- spec/stripe_tester_spec.rb
+++ spec/stripe_tester_spec.rb
@@ -26,6 +26,7 @@ describe StripeTester do
 
 		it "#webhook_url should set the default url for the class" do
 			url = 'http://www.google.com'
+			StripeTester.remove_url
 			StripeTester.webhook_url = url
 
 			result_url = StripeTester.webhook_url
@@ -33,20 +34,32 @@ describe StripeTester do
 		end
 
 		it "#webhook_url should store url as and URI object" do
-			url = 'http://www.google.com'
+			url = 'http://www.hello.com'
+			StripeTester.remove_url
 			StripeTester.webhook_url = url
 
 			result_url = StripeTester.webhook_url
 			expect(result_url).to be_a_kind_of(URI)
 		end
 
-		pending "#webhook_url should raise exception when URL is invalid" do
+		it "#webhook_url should not store URL when URL is invalid" do
+			StripeTester.remove_url
+			StripeTester.webhook_url = 'datatatat'
+			expect(StripeTester.webhook_url).to eq(nil)
 		end
 
 		pending "#post_to_url should send data to url" do 
+			
 		end
 
-		pending "#override_attributes should override attributes in default data to custom data" do
+		it "#override_attributes should override attributes in default data to custom data" do
+			original_data = {name: 'john smith', info: {age: 45, gender: 'male'}}
+			options = {name: 'smith john', age: 99}
+
+			StripeTester.override_attributes(original_data, options)
+
+			age = original_data[:info][:age]
+			expect(age).to eq(options[:age])
 		end
 	end
 end
