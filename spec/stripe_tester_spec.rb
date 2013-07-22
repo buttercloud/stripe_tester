c982f8806777dd35a22e46ad4b77bfd253caf4c5
Luay Bseiso <luay@buttercloud.com>
2013-07-22 16:26:28 +0300

    wrote more tests + change tabs to spaces -- refs #14

--- spec/stripe_tester_spec.rb
+++ spec/stripe_tester_spec.rb
@@ -2,75 +2,94 @@ require 'spec_helper'
 
 describe StripeTester do
 
-	describe "instance methods" do
-		it "#load_template should return hash" do
-			result = StripeTester.load_template(:invoice_created)
+  describe "instance methods" do
+    it "#load_template should return hash" do
+      result = StripeTester.load_template(:invoice_created)
 
-			expect(result).to be_a_kind_of(Hash)
-		end
+      expect(result).to be_a_kind_of(Hash)
+    end
 
-		it "#load_template should return correct callback type" do
-			type = "invoice_created"
+    it "#load_template should return correct callback type" do
+      type = "invoice_created"
 
-			returned_hash = StripeTester.load_template(type)
-			returned_type = returned_hash["type"]
+      returned_hash = StripeTester.load_template(type)
+      returned_type = returned_hash["type"]
 
-			returned_type.sub!('.', '_')
+      returned_type.sub!('.', '_')
 
-			expect(returned_type).to eq(type)
-		end
+      expect(returned_type).to eq(type)
+    end
 
-		it "#load_template should return nil  when invalid event is given" do
-			type = "incorrect_type"
+    it "#load_template should raise an exception when invalid event is given" do
+      type = "incorrect_type"
 
-			returned_hash = StripeTester.load_template(type)
+      expect { StripeTester.load_template(type) }.to raise_error
+    end
 
-			expect(returned_hash).to eq(nil)
-		end
+    it "#webhook_url should set the default url for the class" do
+      url = 'http://www.google.com'
+      StripeTester.remove_url
+      StripeTester.webhook_url = url
 
-		it "#webhook_url should set the default url for the class" do
-			url = 'http://www.google.com'
-			StripeTester.remove_url
-			StripeTester.webhook_url = url
+      result_url = StripeTester.webhook_url
+      expect(result_url.to_s).to eq(url)
+    end
 
-			result_url = StripeTester.webhook_url
-			expect(result_url.to_s).to eq(url)
-		end
+    it "#webhook_url should store url as and URI object" do
+      url = 'http://www.hello.com'
+      StripeTester.remove_url
+      StripeTester.webhook_url = url
 
-		it "#webhook_url should store url as and URI object" do
-			url = 'http://www.hello.com'
-			StripeTester.remove_url
-			StripeTester.webhook_url = url
+      result_url = StripeTester.webhook_url
+      expect(result_url).to be_a_kind_of(URI)
+    end
 
-			result_url = StripeTester.webhook_url
-			expect(result_url).to be_a_kind_of(URI)
-		end
+    it "#webhook_url should not store URL when URL is invalid" do
+      StripeTester.remove_url
+      StripeTester.webhook_url = 'datatatat'
+      expect(StripeTester.webhook_url).to eq(nil)
+    end
 
-		it "#webhook_url should not store URL when URL is invalid" do
-			StripeTester.remove_url
-			StripeTester.webhook_url = 'datatatat'
-			expect(StripeTester.webhook_url).to eq(nil)
-		end
+    it "#post_to_url should send data to url" do
+    end
 
-		pending "#post_to_url should send data to url" do 
-			
-		end
+    it "#post_to_url should raise an error if webhook URL is not set" do
+      StripeTester.remove_url
+      expect { StripeTester.post_to_url() }.to raise_error
+    end
 
-		pending "#override_attributes should override attributes in default data to custom data" do
-			original_data = {name: 'john smith', info: {age: 45, gender: 'male'}}
-			options = {name: 'smith john', age: 99}
+    it "#override_attributes should override attributes in default data to custom data" do
+      original_data = {name: 'john smith', info: {age: 45, gender: 'male'}}
+      override_data = {name: 'smith john', age: 99}
 
-			StripeTester.override_attributes(original_data, options)
+      new_data = StripeTester.override_attributes(original_data, override_data)
 
-			age = original_data[:info][:age]
-			expect(age).to eq(options[:age])
-		end
+      expect(new_data[:name]).to eq(override_data[:name])
+      expect(new_data[:info][:age]).to eq(override_data[:age])
+    end
 
-		it "#find_value should find the value of any value in the hash" do
-			original_data = StripeTester.load_template(:invoice_created)
+    it "#override_attributes should return an unmodified hash when options don't exist" do
+      original_data = {name: 'john smith', info: {age: 45, gender: 'male'}}
 
-			var = StripeTester.find_value(original_data, "url")
-			expect(var).to eq("/v1/invoices/in_27H6EmDDBU0Xza/lines")
-		end
-	end
+      new_data = StripeTester.override_attributes(original_data)
+
+      expect(new_data).to eq(original_data)
+    end
+
+    it "#replace_value should replace a value of a given key in the hash" do
+      original_data = {name: 'john smith', info: {age: 45, gender: 'male'}}
+
+      new_data = StripeTester.replace_value(original_data, :age, 99)
+
+      expect(new_data[:info][:age]).to eq(99)
+    end
+
+    it "#create_event should send the event to the URL with the modified hash if options exist" do
+      
+    end
+
+    it "#create_event should send the event with an unmodified hash to the URL if options don't exist" do
+      
+    end
+  end
 end
