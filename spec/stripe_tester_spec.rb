c1516ba0db5a9513c188517b031b4501f89c38b2
Luay Bseiso <luay@buttercloud.com>
2014-10-31 17:30:10 +0200

    oops forgot to add version to rspec and wrong version for global var

--- spec/stripe_tester_spec.rb
+++ spec/stripe_tester_spec.rb
@@ -4,7 +4,7 @@ describe StripeTester do
 
   describe "instance methods" do
 
-    LATEST_STRIPE_VERSION = "2013-08-13"
+    LATEST_STRIPE_VERSION = "2014-10-07"
 
     before(:each) do
       StripeTester.remove_url
