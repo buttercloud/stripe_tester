b0037292863e05a49c0095e0dd8dcbb91a374104
Luay Bseiso <luay@buttercloud.com>
2015-06-24 14:00:46 +0300

    update test to use the latest stripe version

--- spec/stripe_tester_spec.rb
+++ spec/stripe_tester_spec.rb
@@ -4,7 +4,7 @@ describe StripeTester do
 
   describe "instance methods" do
 
-    LATEST_STRIPE_VERSION = "2014-10-07"
+    LATEST_STRIPE_VERSION = "2015-04-07"
 
     before(:each) do
       StripeTester.remove_url
